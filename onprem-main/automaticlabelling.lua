98089876472aeceecec77433979c3946dcf96524
holakunle <holakunle69@gmail.com>
2025-03-04 19:46:19 +0100

    autolabeling script to include error handling

--- onprem-main/automaticlabelling.lua
+++ onprem-main/automaticlabelling.lua
@@ -1,9 +1,23 @@
 function OnStoredInstance(instanceId, tags, metadata)
-    -- Add "NEW" label to new studies
-    SetLabel(instanceId, "NEW")
-  end
-  
-  function OnModifiedInstance(instanceId, tags, metadata)
-    -- Add "MODIFIED" label to edited studies
-    SetLabel(instanceId, "MODIFIED")
-  end
+    local studyId = GetParentResource(instanceId, "Study")
+    if studyId then
+        local success, err = pcall(SetLabel, studyId, "NEW")
+        if not success then
+            print("Error setting label for stored instance: " .. err)
+        end
+    else
+        print("Study ID not found for stored instance: " .. instanceId)
+    end
+end
+
+function OnModifiedInstance(instanceId, tags, metadata, origin)
+    local studyId = GetParentResource(instanceId, "Study")
+    if studyId then
+        local success, err = pcall(SetLabel, studyId, "MODIFIED")
+        if not success then
+            print("Error setting label for modified instance: " .. err)
+        end
+    else
+        print("Study ID not found for modified instance: " .. instanceId)
+    end
+end
